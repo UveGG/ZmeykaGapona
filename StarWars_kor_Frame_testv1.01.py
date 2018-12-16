2bf91016416374397a19655f0bddba871e722e34
NikGapon <45200250+NikGapon@users.noreply.github.com>
2018-12-16 11:11:15 +0400

    test

--- StarWars_kor_Frame_testv1.01.py
+++ StarWars_kor_Frame_testv1.01.py
@@ -1,11 +1,9 @@
 import sys
 
 from PyQt5 import QtCore
-
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QLabel, QWidget
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor, QPixmap
-
 
 
 
@@ -18,26 +16,29 @@ class StarWars(QMainWindow):
         self.setWindowTitle('StarWars')
         self.resize(750, 800)  # Размеры окна
         self.center()  # Центрируем игру
-        self.fone()  # Постановка фона
-        self.show()
+        #self.fone()  # Постановка фона
+
         #решение вопроса с фреймом
         #-self.kor = QtWidgets.QFrame(self.centralwidget)
         self.x_ss = 350
-        self.y_ss = 700
+        self.y_ss = 350
         self.spaceship = QFrame(self)
+        #self.spaceship.setGeometry(self.x_ss, self.y_ss, 50, 50)
+
         self.spaceship.setGeometry(QtCore.QRect(self.x_ss, self.y_ss, 50, 50))
 
+        #self.spaceship.setStyleSheet("background-color: red")
 
-        #self.spaceship.setFrameStyle(QFrame::StyledPanel);
+        #self.spaceship.setStyleSheet("background-image: url(:/truck/test_spaceship_v2.png);background-repeat: no-repeat; ")
+
+        #self.spaceship.setFrameStyle(QFrame::StyledPanel)
 
         #self.spaceship.setStyleSheet("background-image: url(C:\Yandex_L\ZmeykaGapona\test_spaceship_v2.png)")
-        #self.spaceship.setStyleSheet("background:black;background-position: center; background-image: url(:/truck/image.png);background-repeat: no-repeat; ")
+        #self.spaceship.setStyleSheet("background:black;background-position: center; background-image: url(:/truck/test_spaceship_v2.png);background-repeat: no-repeat; ")
+
         self.show()
 
-        #-self.kor.setAutoFillBackground(False)
-        #-self.kor.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        #-self.kor.setFrameShadow(QtWidgets.QFrame.Raised)
-        #-self.kor.setObjectName("kor")
+
         #-----------------------------------------------
     def fone(self):  # Создали лейбл, наложили как фон
         label = QLabel(self)
